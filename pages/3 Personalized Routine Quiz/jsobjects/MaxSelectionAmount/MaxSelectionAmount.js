9c6dd7cb8a668a831b23d531074ca6df645707fa
karuna.adhikari <karuna.adhikari@mail.utoronto.ca>
2025-03-02 02:19:20 +0000

    Theme + Q3

--- pages/3 Personalized Routine Quiz/jsobjects/MaxSelectionAmount/MaxSelectionAmount.js
+++
@@ -1,7 +0,0 @@
-export default {
-	Button1onClick () {
-		if (CheckboxGroup1.selectedValues.length > 3) {
-			CheckboxGroup1.setSelectedOptions(CheckboxGroup1.selectedValues.slice(0, 3));
-		}
-	}
-}
